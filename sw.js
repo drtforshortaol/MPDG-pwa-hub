6a1dc9151ebb2d8d0a59f289bd9310c29dc21a4b
Glenn Takenaga DDS. MSc <drtforshort@aol.com>
2026-06-20 21:52:40 -0700

    Update service worker for hub and planner

--- sw.js
+++ sw.js
@@ -1,5 +1,28 @@
-const CACHE='dental-treatment-planner-v11';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./codes.js','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE = "mpdg-hub-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./hub-styles.css",
+  "./hub-app.js",
+  "./apps-config.js",
+  "./treatment-planner.html",
+  "./styles.css",
+  "./app.js",
+  "./codes.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
