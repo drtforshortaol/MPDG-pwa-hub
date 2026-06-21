2759f62aecd7beeb00298dbf486f9d8993e0c34c
Glenn Takenaga DDS. MSc <drtforshort@aol.com>
2026-06-20 22:58:58 -0700

    Add PWA template service worker

---
+++ templates/pwa-app-template/sw.js
@@ -0,0 +1,24 @@
+const CACHE = "app-template-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./data.js",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
